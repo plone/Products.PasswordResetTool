1f9ef8a5278f59e63f22494ca736f3db3ee02b17
Rob Miller <ra@burningman.com>
2004-12-15 05:20:35 +0000

    * changed pwreset_action to gracefully redirect to portal home when it   doesn't receive the expected parameters.  this prevents error messages   from showing up for users who get redirected back to pwreset_action   when logging in after a successful password change
    svn path=/trunk/; revision=9789

--- Products/PasswordResetTool/skins/PasswordReset/pwreset_action.py
+++ Products/PasswordResetTool/skins/PasswordReset/pwreset_action.py
@@ -5,15 +5,24 @@
 ##bind script=script
 ##bind subpath=traverse_subpath
 ##title=Reset a user's password
-##parameters=randomstring, userid, password, password2
+##parameters=
+req = context.REQUEST
+fparams = {'randomstring':'', 'userid':'',
+           'password':'', 'password2':'',}
+for fp in fparams:
+    fparams[fp] = req.get(fp, '')
+    if not fparams[fp]:
+        req.RESPONSE.redirect(context.portal_url())
 
-failMessage=context.portal_registration.testPasswordValidity(password, password2)
+failMessage=context.portal_registration.testPasswordValidity(fparams['password'],
+                                                             fparams['password2'])
 if failMessage:
     return context.pwreset_form(portal_status_message=failMessage)
 
 try:
     pw_tool = context.portal_password_reset
-    pw_tool.resetPassword(userid, randomstring, password)
+    pw_tool.resetPassword(fparams['userid'], fparams['randomstring'],
+                          fparams['password'])
     status = "done"
 except 'ExpiredRequestError':
     status = "expired"
