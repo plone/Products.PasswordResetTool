b20c212a87052cd768fab27172780f9cbd6325fd
Daniel Nouri <daniel.nouri@gmail.com>
2007-06-03 14:16:13 +0000

    - Fix `RegistrationTool.mailPassword` by passing the results of   `reset_tool.requestReset` to it, instead of having the template   call the tool's function.  Also take care about encoding, since   the template returns unicode now.  This makes e-mailing   forgotten passwords work again, and fixes   http://dev.plone.org/plone/ticket/6614
    svn path=/PasswordResetTool/trunk/; revision=43073

--- Products/PasswordResetTool/tests/utils.py
+++ Products/PasswordResetTool/tests/utils.py
@@ -1,4 +1,3 @@
-import email
 from Products.MailHost.MailHost import MailHost as MailBase
 
 class MockMailHost(MailBase):
@@ -19,14 +18,7 @@ class MockMailHost(MailBase):
         Basically construct an email.Message from the given params to make sure
         everything is ok and store the results in the messages instance var.
         """
-
-        message = email.message_from_string(message)
-        message['To'] = mto
-        message['From'] = mfrom
-        message['Subject'] = subject
-        
         self.messages.append(message)
-        self._p_changed = True
 
     def validateSingleEmailAddress(self, address):
         return True # why not
