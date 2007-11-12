36a99b92ad9620c56487598c8fd8ba84fed65118
Wichert Akkerman <wichert@wiggy.net>
2007-11-12 19:56:38 +0000

    New place
    svn path=/Products.PasswordResetTool/trunk/; revision=53687

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+version = '1.1'
+
+setup(name='Products.PasswordResetTool',
+      version=version,
+      description="password reset tool for Plone",
+      long_description="""\
+""",
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='password reset plone',
+      author='J. Cameron Cooper',
+      author_email='prt@jcameroncooper.com',
+      url='',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
