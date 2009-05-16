3d450dc612e7dd9064b5b7e60db8ddfb6ee5c7c4
Wichert Akkerman <wichert@wiggy.net>
2009-05-16 17:07:08 +0000

    Syntax
    svn path=/Products.PasswordResetTool/branches/1.x/; revision=86255

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.2'
 setup(name='Products.PasswordResetTool',
       version=version,
       description="password reset tool for Plone",
-      long_description=open("README.txt").read() + "\n"
+      long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       classifiers=[
         "Framework :: Plone",
