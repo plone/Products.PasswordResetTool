fd9ae1c52f700e15824ca8d533edbfcceab2db56
Radim Novotny <novotny.radim@gmail.com>
2009-11-11 12:37:55 +0000

    Plone4 tests fix in progress
    svn path=/Products.PasswordResetTool/trunk/; revision=101908

--- Products/PasswordResetTool/tests/test_doctests.py
+++ Products/PasswordResetTool/tests/test_doctests.py
@@ -6,26 +6,38 @@ import doctest
 import unittest
 from Testing.ZopeTestCase import FunctionalDocFileSuite
 from Products.PloneTestCase import PloneTestCase
+from Products.MailHost.interfaces import IMailHost
+from zope.component import getSiteManager
+from Acquisition import aq_base
 
 PloneTestCase.setupPloneSite()
 
 from Products.PasswordResetTool.tests.utils import MockMailHost
 
 
+OPTIONFLAGS = (doctest.ELLIPSIS |
+               doctest.NORMALIZE_WHITESPACE)
+
 class MockMailHostTestCase(PloneTestCase.FunctionalTestCase):
 
     def afterSetUp(self):
         self.portal._original_MailHost = self.portal.MailHost
-        self.portal.MailHost = MockMailHost('MailHost')
+        self.portal.MailHost = mailhost = MockMailHost('MailHost')
+        sm = getSiteManager(context=self.portal)
+        sm.unregisterUtility(provided=IMailHost)
+        sm.registerUtility(mailhost, provided=IMailHost)
 
     def beforeTearDown(self):
         self.portal.MailHost = self.portal._original_MailHost
+        sm = getSiteManager(context=self.portal)
+        sm.unregisterUtility(provided=IMailHost)
+        sm.registerUtility(aq_base(self.portal._original_MailHost), provided=IMailHost)
 
 
 def test_suite():
     return unittest.TestSuite((
         FunctionalDocFileSuite('browser.txt',
-                               optionflags = doctest.REPORT_ONLY_FIRST_FAILURE,
+                               optionflags=OPTIONFLAGS,
                                package='Products.PasswordResetTool.tests',
                                test_class=MockMailHostTestCase),
         ))
--- Products/PasswordResetTool/tests/utils.py
+++ Products/PasswordResetTool/tests/utils.py
@@ -1,26 +1,25 @@
 from Products.MailHost.MailHost import MailHost as MailBase
+from Products.MailHost.MailHost import _mungeHeaders
 from persistent.list import PersistentList
 
 
+# copied from Products.CMFPlone.tests.utils
+
 class MockMailHost(MailBase):
     """A MailHost that collects messages instead of sending them.
-
-    Thanks to Rocky Burt for inspiration.
     """
-    
+
     def __init__(self, id):
-        MailBase.__init__(self, id)
         self.reset()
-    
+
     def reset(self):
         self.messages = PersistentList()
-    
-    def send(self, message, mto=None, mfrom=None, subject=None, encode=None):
-        """
-        Basically construct an email.Message from the given params to make sure
-        everything is ok and store the results in the messages instance var.
-        """
-        self.messages.append(message)
 
-    def validateSingleEmailAddress(self, address):
-        return True # why not
+    def send(self, messageText, mto=None, mfrom=None, subject=None,
+             encode=None, immediate=False, charset=None, msg_type=None):
+        messageText, mto, mfrom = _mungeHeaders(messageText,
+                                                mto, mfrom, subject,
+                                                charset=charset,
+                                                msg_type=msg_type)
+        self.messages.append(messageText)
+
