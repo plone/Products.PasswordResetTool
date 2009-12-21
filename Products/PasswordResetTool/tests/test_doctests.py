333215e856c5e2cba7bb6c6d8fc0c284291b0ecd
Maurits van Rees <maurits@vanrees.org>
2009-12-21 23:00:57 +0000

    Partly revert r106540 to avoid depending on a change in Plone branch 3.3 for fixing a test failure (and needing a new Plone release for that). In the mail tests, properly register a mock secure mail host, like the MockMailHostTestCase in CMFPlone does now, including a mocked secureSend method.
    svn path=/Products.PasswordResetTool/branches/1.x/; revision=107189

--- Products/PasswordResetTool/tests/test_doctests.py
+++ Products/PasswordResetTool/tests/test_doctests.py
@@ -6,15 +6,35 @@ import doctest
 import unittest
 from Testing.ZopeTestCase import FunctionalDocFileSuite
 from Products.PloneTestCase import PloneTestCase
-from Products.CMFPlone.tests.test_mails import MockMailHostTestCase
+from zope.component import getSiteManager
+from Acquisition import aq_base
+from Products.MailHost.interfaces import IMailHost
+from Products.PasswordResetTool.tests.utils import MockMailHost
 
 PloneTestCase.setupPloneSite()
+
+
+class MockMailHostTestCase(PloneTestCase.FunctionalTestCase):
+
+    def afterSetUp(self):
+        self.portal._original_MailHost = self.portal.MailHost
+        self.portal.MailHost = mailhost = MockMailHost('MailHost')
+        sm = getSiteManager(context=self.portal)
+        sm.unregisterUtility(provided=IMailHost)
+        sm.registerUtility(mailhost, provided=IMailHost)
+
+    def beforeTearDown(self):
+        self.portal.MailHost = self.portal._original_MailHost
+        sm = getSiteManager(context=self.portal)
+        sm.unregisterUtility(provided=IMailHost)
+        sm.registerUtility(aq_base(self.portal._original_MailHost),
+                           provided=IMailHost)
 
 
 def test_suite():
     return unittest.TestSuite((
         FunctionalDocFileSuite('browser.txt',
-                               optionflags = doctest.REPORT_ONLY_FIRST_FAILURE,
+                               optionflags=doctest.REPORT_ONLY_FIRST_FAILURE,
                                package='Products.PasswordResetTool.tests',
                                test_class=MockMailHostTestCase),
         ))
