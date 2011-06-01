9b76b56a4cda79884283e2badd371ca17ae7ae82
Laurence Rowe <l@lrowe.co.uk>
2011-06-01 16:44:01 +0000

    Apply patch to prevent exploitation of CVE-2011-1948 (Hotfix 20110531.)
    svn path=/Products.PasswordResetTool/trunk/; revision=240283

--- Products/PasswordResetTool/skins/PasswordReset/pwreset_constructURL.py
+++ Products/PasswordResetTool/skins/PasswordReset/pwreset_constructURL.py
@@ -6,5 +6,9 @@
 ##bind subpath=traverse_subpath
 ##title=Create the URL where passwords are reset
 ##parameters=randomstring
+from zExceptions import Forbidden
+if container.REQUEST['PUBLISHED'] is script:
+   raise Forbidden('Script may not be published.')
+
 host = container.absolute_url()
 return "%s/passwordreset/%s" % (host, randomstring)
