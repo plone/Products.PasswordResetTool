20eff57285ab1cc479516dc5ff135516e50f29b4
Tom Gross <itconsense@gmail.com>
2014-10-31 08:47:00 +0000

    readded teardown and put layers around doctests

--- Products/PasswordResetTool/tests/test_doctests.py
+++ Products/PasswordResetTool/tests/test_doctests.py
@@ -11,6 +11,7 @@ from Acquisition import aq_base
 from Products.CMFPlone.tests.utils import MockMailHost
 from plone.app import testing
 from plone.testing import layered
+from transaction import commit
 
 OPTIONFLAGS = (doctest.ELLIPSIS |
                doctest.NORMALIZE_WHITESPACE)
@@ -20,30 +21,34 @@ class MockMailFixture(testing.PloneSandboxLayer):
     defaultBases = (testing.PLONE_FIXTURE,)
 
     def setUpPloneSite(self, portal):
-        portal._original_MailHost = self.portal.MailHost
+        portal._original_MailHost = portal.MailHost
         portal.MailHost = mailhost = MockMailHost('MailHost')
         mailhost.smtp_host = 'localhost'
         sm = getSiteManager(context=portal)
         sm.unregisterUtility(provided=IMailHost)
         sm.registerUtility(mailhost, provided=IMailHost)
         portal.email_from_address = 'test@example.com'
+        commit()
 
-#    def beforeTearDown(self):
-#        self.portal.MailHost = self.portal._original_MailHost
-#        sm = getSiteManager(context=self.portal)
-#        sm.unregisterUtility(provided=IMailHost)
-#        sm.registerUtility(aq_base(self.portal._original_MailHost), provided=IMailHost)
+    def tearDownPloneSite(self, portal):
+        portal.MailHost = portal._original_MailHost
+        sm = getSiteManager(context=portal)
+        sm.unregisterUtility(provided=IMailHost)
+        sm.registerUtility(aq_base(portal._original_MailHost), provided=IMailHost)
+
 
 MOCK_MAIL_FIXTURE = MockMailFixture()
+MM_FUNCTIONAL_TESTING = testing.FunctionalTesting(
+            bases=(MOCK_MAIL_FIXTURE,), name='PloneTestCase:Functional')
+
 
 def test_suite():
     return unittest.TestSuite((
-        doctest.DocFileSuite('browser.txt',
-                               optionflags=OPTIONFLAGS,
-                               package='Products.PasswordResetTool.tests',
-                               test_class=MockMailHostTestCase),
-        doctest.DocFileSuite('view.txt',
-                               optionflags=OPTIONFLAGS,
-                               package='Products.PasswordResetTool.tests',
-                               test_class=MockMailHostTestCase),
-        ))
+        layered(doctest.DocFileSuite('browser.txt',
+            optionflags=OPTIONFLAGS,
+            package='Products.PasswordResetTool.tests',),
+            layer=MM_FUNCTIONAL_TESTING),
+        layered(doctest.DocFileSuite('view.txt',
+            optionflags=OPTIONFLAGS,
+            package='Products.PasswordResetTool.tests',),
+            layer=MM_FUNCTIONAL_TESTING)))
