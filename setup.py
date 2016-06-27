d8a3aa37c02d8e85222cd8fa00364d57396b758d
Maurits van Rees <maurits@vanrees.org>
2016-06-27 09:59:25 +0200

    Reindented setup.py, added classifiers.
    [ci skip]

--- setup.py
+++ setup.py
@@ -2,42 +2,50 @@ from setuptools import setup, find_packages
 
 version = '2.0.19.dev0'
 
-setup(name='Products.PasswordResetTool',
-      version=version,
-      description="Password reset tool for Plone",
-      long_description=open("README.txt").read() + "\n" + \
-              open("CHANGES.txt").read(),
-      classifiers=[
+setup(
+    name='Products.PasswordResetTool',
+    version=version,
+    description="Password reset tool for Plone",
+    long_description=(
+        open("README.txt").read() + "\n" +
+        open("CHANGES.txt").read()),
+    classifiers=[
         "Framework :: Plone",
+        "Framework :: Plone :: 4.0",
+        "Framework :: Plone :: 4.1",
+        "Framework :: Plone :: 4.2",
+        "Framework :: Plone :: 4.3",
         "Framework :: Zope2",
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "Programming Language :: Python",
-        ],
-      keywords='password reset plone',
-      author='J. Cameron Cooper',
-      author_email='prt@jcameroncooper.com',
-      url='http://pypi.python.org/pypi/Products.PasswordResetTool',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['Products'],
-      include_package_data=True,
-      zip_safe=False,
-      extras_require=dict(
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+    ],
+    keywords='password reset plone',
+    author='J. Cameron Cooper',
+    author_email='prt@jcameroncooper.com',
+    url='https://pypi.python.org/pypi/Products.PasswordResetTool',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['Products'],
+    include_package_data=True,
+    zip_safe=False,
+    extras_require=dict(
         test=[
             'plone.app.testing',
-        ]
-      ),
-      install_requires=[
-          'setuptools',
-          'plone.memoize',
-          'zope.component',
-          'zope.i18nmessageid',
-          'zope.i18n',
-          'zope.interface',
-          'Products.CMFPlone',
-          'Products.CMFCore',
-          'Acquisition',
-          'DateTime',
-          'Zope2',
-      ],
-      )
+        ],
+    ),
+    install_requires=[
+        'setuptools',
+        'plone.memoize',
+        'zope.component',
+        'zope.i18nmessageid',
+        'zope.i18n',
+        'zope.interface',
+        'Products.CMFPlone',
+        'Products.CMFCore',
+        'Acquisition',
+        'DateTime',
+        'Zope2',
+    ],
+    )
